d0851e3ffed94c727b2a51634ab1b07dd8a368ef
ahmad <ahmadkadi501@gmail.com>
2025-02-28 23:02:48 +0200

    printed total number of employee in the company

--- test.py
+++ test.py
@@ -16,3 +16,12 @@ print(company_employees)
 company_employees["Engineering"]["David"] = {"age": 27, "role": "Data Scientist"}
 print("\nUpdated company_employees dictionary with new employee:")
 print(company_employees)
+
+def count_total_employees(company_dict):
+    total_count = 0
+    for department, employees in company_dict.items():
+        total_count += len(employees)
+    return total_count
+
+total_employees = count_total_employees(company_employees)
+print("\nTotal number of employees in the company:", total_employees)
